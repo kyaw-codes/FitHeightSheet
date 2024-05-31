baa20ec454a48b6265b0ada9ca2a80dbea06fa29
Kyaw Monkey <kyaw.codes@gmail.com>
2024-05-31 22:00:08 +0700

    fix: - [x] Drag to dismiss yOffset problem

--- Sources/FitHeightSheet/FitHeightSheet.swift
+++ Sources/FitHeightSheet/FitHeightSheet.swift
@@ -6,7 +6,7 @@ extension View {
     isPresented: Binding<Bool>,
     backdropColor: Color = .black,
     backdropOpacity: CGFloat = 0.5,
-    topContentOffset: CGFloat = 40,
+    topContentInset: CGFloat = 40,
     @ViewBuilder _ body: @escaping () -> Body
   ) -> some View {
     modifier(
@@ -14,7 +14,7 @@ extension View {
         isPresented: isPresented,
         backdropColor: backdropColor,
         backdropOpacity: backdropOpacity,
-        topContentOffset: topContentOffset,
+        topContentInset: topContentInset,
         body
       )
     )
@@ -41,31 +41,42 @@ struct FitHeightSheetDismissKey: EnvironmentKey {
 struct FitHeightSheetModifire<Body: View>: ViewModifier {
   @Binding var isPresented: Bool
   @State private var internalPresented: Bool = false
+  @State private var dragOffsetY = CGFloat.zero
   @State private var offsetY = CGFloat.zero
   @State private var contentHeight = CGFloat.zero
   @State private var keyboardHeight: CGFloat = 0
   
   private let backdropColor: Color
   private let backdropOpacity: CGFloat
-  private let topContentOffset: CGFloat
+  private let topContentInset: CGFloat
   
-  var body: () -> Body
+  private var body: () -> Body
   
-  var safeAreaTop: CGFloat {
-    UIApplication.shared.connectedScenes.filter { $0.activationState == .foregroundActive }.first(where: { $0 is UIWindowScene }).flatMap({ $0 as? UIWindowScene })?.windows.first(where: \.isKeyWindow)?.safeAreaInsets.top ?? 0
+  private var safeAreaTop: CGFloat {
+    UIApplication.shared.connectedScenes
+      .filter { $0.activationState == .foregroundActive }
+      .first(where: { $0 is UIWindowScene })
+      .flatMap({ $0 as? UIWindowScene })?
+      .windows
+      .first(where: \.isKeyWindow)?
+      .safeAreaInsets.top ?? 0
+  }
+  
+  private var availableContentHeight: CGFloat {
+    UIScreen.main.bounds.height - topContentInset - safeAreaTop
   }
   
   init(
     isPresented: Binding<Bool>,
     backdropColor: Color,
     backdropOpacity: CGFloat,
-    topContentOffset: CGFloat,
+    topContentInset: CGFloat,
     @ViewBuilder _ body: @escaping () -> Body
   ) {
     self._isPresented = isPresented
     self.backdropColor = backdropColor
     self.backdropOpacity = backdropOpacity
-    self.topContentOffset = topContentOffset
+    self.topContentInset = topContentInset
     self.body = body
   }
   
@@ -91,16 +102,22 @@ struct FitHeightSheetModifire<Body: View>: ViewModifier {
         body()
           .opacity(isPresented ? 1 : 0)
           .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
-          .padding(.top, topContentOffset)
+          .padding(.top, topContentInset)
           .background(
             GeometryReader {
-              Color.clear.preference(key: ContentHeightKey.self, value: $0.size.height)
+              Color.clear
+                .preference(key: ContentHeightKey.self, value: $0.size.height)
+                .preference(key: OffsetYKey.self, value: $0.frame(in: .global).minY)
             }
           )
           .onPreferenceChange(ContentHeightKey.self) {
             contentHeight = $0
           }
-          .offset(y: max(0, offsetY))
+          .onPreferenceChange(OffsetYKey.self) { offsetY in
+            let height = UIScreen.main.bounds.height - min(availableContentHeight, contentHeight) - topContentInset
+            self.offsetY = offsetY - height
+          }
+          .offset(y: max(0, dragOffsetY))
           .padding(.bottom, keyboardHeight > 0 ? keyboardHeight : 0)
           .onReceive(Publishers.keyboardHeight) { height in
             withAnimation(isPresented ? .smooth : .bouncy) {
@@ -110,19 +127,19 @@ struct FitHeightSheetModifire<Body: View>: ViewModifier {
           .gesture(
             DragGesture(minimumDistance: 5)
               .onChanged { value in
-                offsetY = value.translation.height
+                dragOffsetY = value.translation.height
               }
               .onEnded { value in
-                if value.location.y > (contentHeight * 0.5) {
+                if offsetY > (contentHeight * 0.5) {
                   withAnimation(isPresented ? .smooth : .bouncy) {
                     isPresented.toggle()
                   }
                   DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
-                    offsetY = 0
+                    dragOffsetY = 0
                   }
                 } else {
                   withAnimation(isPresented ? .smooth : .bouncy) {
-                    offsetY = 0
+                    dragOffsetY = 0
                   }
                 }
               }
@@ -145,6 +162,12 @@ struct FitHeightSheetModifire<Body: View>: ViewModifier {
 }
 
 private struct ContentHeightKey: PreferenceKey {
+  static var defaultValue: CGFloat = .zero
+  
+  static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {}
+}
+
+private struct OffsetYKey: PreferenceKey {
   static var defaultValue: CGFloat = .zero
   
   static func reduce(value: inout CGFloat, nextValue: () -> CGFloat) {}
