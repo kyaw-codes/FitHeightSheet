4689af23d25946c3248a1095148670338ccbf2b8
Kyaw Monkey <kyaw.codes@gmail.com>
2024-06-12 21:44:37 +0700

    chore: updated the example code to tabview

--- FitHeightSheetDemo/FitHeightSheetDemo/ContentView.swift
+++ FitHeightSheetDemo/FitHeightSheetDemo/ContentView.swift
@@ -8,6 +8,37 @@
 import SwiftUI
 import FitHeightSheet
 
+struct MainView: View {
+  @State private var currentTab = 0
+  var body: some View {
+    TabView(selection: $currentTab) {
+      ContentView()
+        .tag(0)
+        .tabItem {
+          Label("Home", systemImage: "house")
+        }
+      
+      ContentView()
+        .tag(0)
+        .tabItem {
+          Label("Profile", systemImage: "person")
+        }
+      
+      ContentView()
+        .tag(0)
+        .tabItem {
+          Label("Search", systemImage: "magnifyingglass")
+        }
+      
+      ContentView()
+        .tag(0)
+        .tabItem {
+          Label("Settings", systemImage: "gear")
+        }
+    }
+  }
+}
+
 struct ContentView: View {
   @Environment(\.fitHeightSheetDismiss) private var dismiss
   @State private var showSheet = false
@@ -41,8 +72,8 @@ struct ContentView: View {
       Text("Hello there 👋")
         .font(.title.bold())
       
-      TextField("Type something here...", text: $text)
-//      Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla varius consectetur pellentesque. Praesent ornare velit sit amet lectus egestas, vitae condimentum ex tincidunt. Proin ut tincidunt nisl. Suspendisse sapien quam, vulputate eu vestibulum in, sagittis in sem. Suspendisse nibh ante.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla varius consectetur pellentesque. Praesent ornare velit sit amet lectus egestas, vitae condimentum ex tincidunt. Proin ut tincidunt nisl. Suspendisse sapien quam, vulputate eu vestibulum in, sagittis in sem. Suspendisse nibh ante.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla varius consectetur pellentesque. Praesent ornare velit sit amet lectus egestas, vitae condimentum ex tincidunt. Proin ut tincidunt nisl. Suspendisse sapien quam, vulputate eu vestibulum in, sagittis in sem. Suspendisse nibh ante.")
+//      TextField("Type something here...", text: $text)
+      Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla varius consectetur pellentesque. Praesent ornare velit sit amet lectus egestas, vitae condimentum ex tincidunt. Proin ut tincidunt nisl. Suspendisse sapien quam, vulputate eu vestibulum in, sagittis in sem. Suspendisse nibh ante.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla varius consectetur pellentesque. Praesent ornare velit sit amet lectus egestas, vitae condimentum ex tincidunt. Proin ut tincidunt nisl. Suspendisse sapien quam, vulputate eu vestibulum in, sagittis in sem. Suspendisse nibh ante.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla varius consectetur pellentesque. Praesent ornare velit sit amet lectus egestas, vitae condimentum ex tincidunt. Proin ut tincidunt nisl. Suspendisse sapien quam, vulputate eu vestibulum in, sagittis in sem. Suspendisse nibh ante.")
       
       Button {
         dismiss()
@@ -66,5 +97,5 @@ struct ContentView: View {
 }
 
 #Preview {
-  ContentView()
+  MainView()
 }
--- FitHeightSheetDemo/FitHeightSheetDemo/FitHeightSheetDemoApp.swift
+++ FitHeightSheetDemo/FitHeightSheetDemo/FitHeightSheetDemoApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct FitHeightSheetDemoApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+          MainView()
         }
     }
 }
