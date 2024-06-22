a7c33b0962911de2e070717f8674d4913a4f29ee
Kyaw Monkey <kyaw.codes@gmail.com>
2024-06-23 01:10:16 +0700

    chore: added documentation for view modifiers

--- Sources/FitHeightSheet/FitHeightSheet.swift
+++ Sources/FitHeightSheet/FitHeightSheet.swift
@@ -2,6 +2,22 @@ import SwiftUI
 import Combine
 
 extension View {
+  /**
+   The `fitHeightSheet` function is a SwiftUI view modifier that presents a custom sheet with specific height constraints. It provides various customization options including animations, backdrop style, and dismissal behavior.
+   
+    - Parameters:
+      - isPresented: A `Binding<Bool>` that controls whether the sheet is presented (`true`) or dismissed (`false`).
+      - backdropStyle: A `BackdropStyle` that defines the appearance of the backdrop. Defaults to `.default`, which is a semi-transparent black backdrop.
+      - presentAnimation: An `AnimationConfiguration` that specifies the animation to use when presenting the sheet. Defaults to a `.smooth` animation.
+      - dismissAnimation: An `AnimationConfiguration` that specifies the animation to use when dismissing the sheet. Defaults to a `.bouncy` animation.
+      - topContentInset: A `CGFloat` value that sets the top content inset for the sheet. Defaults to `40`.
+      - dismissThreshold: A `CGFloat` value representing the threshold for dismissing the sheet, relative to the height of the sheet. Defaults to `0.5`.
+      - onDismiss: An optional closure `(() -> Void)?` that is called when the sheet is dismissed. Defaults to `nil`.
+      - content: A `ViewBuilder` that creates the content of the sheet.
+    - Returns: A modified view with the fit height sheet behavior applied.
+   
+   The fitHeightSheet function provides a flexible way to present a sheet with a specific height in a SwiftUI application. It allows for detailed customization of the presentation and dismissal animations, backdrop style, and content inset, as well as providing an optional closure for handling the dismissal event.
+   */
   public func fitHeightSheet<Body: View>(
     isPresented: Binding<Bool>,
     backdropStyle: BackdropStyle = .default,
@@ -26,6 +42,18 @@ extension View {
     )
   }
   
+  /** This variant of the function uses an optional item to control the presentation and dismissal of the sheet. It provides various customization options including animations, backdrop style, and dismissal behavior.
+   - Parameters:
+     - item: A `Binding<Item?>` that controls whether the sheet is presented (when the item is non-nil) or dismissed (when the item is nil).
+     - backdropStyle: A `BackdropStyle` that defines the appearance of the backdrop. Defaults to `.default`, which is a semi-transparent black backdrop.
+     - presentAnimation: An `AnimationConfiguration` that specifies the animation to use when presenting the sheet. Defaults to a `.smooth` animation.
+     - dismissAnimation: An `AnimationConfiguration` that specifies the animation to use when dismissing the sheet. Defaults to a `.bouncy` animation.
+     - topContentInset: A `CGFloat` value that sets the top content inset for the sheet. Defaults to `40`.
+     - dismissThreshold: A `CGFloat` value representing the threshold for dismissing the sheet, relative to the height of the sheet. Defaults to `0.5`.
+     - onDismiss: An optional closure `(() -> Void)?` that is called when the sheet is dismissed. Defaults to `nil`.
+     - content: A `ViewBuilder` that creates the content of the sheet.
+   - Returns: A modified view with the fit height sheet behavior applied.
+   */
   public func fitHeightSheet<Body: View, Item>(
     item: Binding<Item?>,
     backdropStyle: BackdropStyle = .default,
